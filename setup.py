f2e2412846e8c39b187d6b0ea377b59ca678f822
Marco Martinez <m-martinez@users.noreply.github.com>
2018-10-18 12:29:21 -0700

    Use WeasyPrint RC

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ DEVELOP = list(yield_packages('requirements-develop.txt'))
 
 setup(
     name='pydf',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
